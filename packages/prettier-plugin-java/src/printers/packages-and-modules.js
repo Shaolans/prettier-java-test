c1413f71ebe2d0ef33314223cb866688c7d4e689
Ling-Chun SO <lingchun.so@gmail.com>
2020-05-06 12:16:59 +0200

    test

--- packages/prettier-plugin-java/src/printers/packages-and-modules.js
+++ packages/prettier-plugin-java/src/printers/packages-and-modules.js
@@ -51,7 +51,9 @@ class PackagesAndModulesPrettierVisitor {
     return rejectAndConcat([
       rejectAndJoin(concat([hardline, hardline]), [
         rejectAndJoin(hardline, staticImports),
+        hardline,hardline,hardline,hardline,hardline,hardline,
         rejectAndJoin(hardline, nonStaticImports),
+        hardline,hardline,hardline,hardline,hardline,hardline,hardline,
         moduleDeclaration
       ])
     ]);
